eea9bdd7cffd55be3fcee72daa00d5630dd18d3c
myleslim <mousee1030@gmail.com>
2025-02-13 10:23:54 +0800

    declared variables within the generate password function

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const includeNumbers = true;
 const includeSymbols = true;    
 
 function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+
+  const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+  const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const numberChars = '0123456789';
+  const symbolChars = '!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
+
   return '';
 }
 
+const password = generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+console.log(`Generated Password: ${password}`);
